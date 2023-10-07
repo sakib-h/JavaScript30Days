9a3211523e2161c244d2affd66b4005189dd0217
Sakib Hasan <sakib100.sa@gmail.com>
2023-10-07 11:35:01 +0600

    day2, problem 2

--- day 2/basicArray.js
+++ day 2/basicArray.js
@@ -7,4 +7,17 @@ function map(arr, fn){
     }
     return transformedArray;
 }
-map([1,2,3],(item,index)=>item+1)
+map([1,2,3],(item,index)=>item+1);
+
+
+// 2634. Filter Elements from Array Based on Callback
+function filter(arr, fn){
+    const filteredArray =[];
+    for(let i=0; i<arr.length; i++){
+        if(fn(arr[i], i)){
+            filteredArray.push(arr[i])
+        }
+    }
+    return filteredArray;
+}
+console.log(filter([1,2,3],(item,index)=>item>1))
