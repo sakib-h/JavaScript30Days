906c04b4c248ae547fcaf45fcaabf4968cab2bd1
Sakib Hasan <sakib100.sa@gmail.com>
2023-10-11 00:31:33 +0600

    day 3,problem 2nd and 3rd

--- day 3/function.js
+++ day 3/function.js
@@ -11,3 +11,26 @@ const compose =(functions)=>{
 }
 
 console.log(compose([Math.sqrt, Math.abs, Math.ceil])(-4)) // 2
+
+// 2703. Return Length of Arguments Passed
+function argumentsLength(...arg){
+    return arg.length
+}
+console.log(argumentsLength(1, 2, 3, 4, 5)) // 5
+
+// 2666. Allow One Function Call
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+function once(fn){
+    let count =0
+    return function(...arg){
+        if(count ===0){
+            count++
+            return fn(...arg)
+        } else {
+            return undefined
+        }
+    }
+}
+console.log(once(Math.max)(1, 2, 3)) // 3
