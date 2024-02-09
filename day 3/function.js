27f1ea9cbe670cbe72e72df08dd86332a34d23a9
Sakib Hasan <sakib100.sa@gmail.com>
2024-02-09 16:03:00 +0600

    problem 2623 solved

--- day 3/function.js
+++ day 3/function.js
@@ -1,48 +1,46 @@
 // 2629. Function Composition
-const compose =(functions)=>{
-    return function(x){
-        functions.reduceRight((total, current)=>{
-            total = current(x)
-            x= total
+const compose = (functions) => {
+    return function (x) {
+        functions.reduceRight((total, current) => {
+            total = current(x);
+            x = total;
+        }, x);
+        return x;
+    };
+};
 
-        }, x)
-        return x
-    }
-}
-
-console.log(compose([Math.sqrt, Math.abs, Math.ceil])(-4)) // 2
+console.log(compose([Math.sqrt, Math.abs, Math.ceil])(-4)); // 2
 
 // 2703. Return Length of Arguments Passed
-function argumentsLength(...arg){
-    return arg.length
+function argumentsLength(...arg) {
+    return arg.length;
 }
-console.log(argumentsLength(1, 2, 3, 4, 5)) // 5
+console.log(argumentsLength(1, 2, 3, 4, 5)); // 5
 
 // 2666. Allow One Function Call
-function once(fn){
-    let count =0
-    return function(...arg){
-        if(count ===0){
-            count++
-            return fn(...arg)
+function once(fn) {
+    let count = 0;
+    return function (...arg) {
+        if (count === 0) {
+            count++;
+            return fn(...arg);
         } else {
-            return undefined
+            return undefined;
         }
-    }
+    };
 }
-console.log(once(Math.max)(1, 2, 3)) // 3
+console.log(once(Math.max)(1, 2, 3)); // 3
 
 //  2623. Memoize
-function memorize(){
-    let cache ={}
-    return function(...arg){
-        let key = JSON.stringify(arg)
-        if(cache[key]){
-            return cache[key]
-        } else {
-            let result = fn(...arg)
-            cache[key] = result
-            return result
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        const key = String(args);
+        if (key in cache) {
+            return cache[key];
         }
-    }
+        const result = fn(...args);
+        cache[key] = result;
+        return result;
+    };
 }
